335024ff66885737ddb5e672a4dc8d8bc3d57632
OleksiiKasian <kasianalex1@gmail.com>
2025-09-09 21:48:04 +0300

    add initial files

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App";
 import "./index.css";
-import App from "./components/App/App.tsx";
-import "modern-normalize";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root") as HTMLDivElement).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>
+  </React.StrictMode>
 );
