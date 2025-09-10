1308e8bafdf030506afbcaca1ce6b2ef5cbf0bf4
OleksiiKasian <kasianalex1@gmail.com>
2025-09-10 18:18:51 +0300

    add initial files

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+const repoName = "03-react-movies";
+
+const isVercel = !!process.env.VERCEL;
+
 export default defineConfig({
   plugins: [react()],
-  base: "/03-react-movies/",
+  base: isVercel ? "/" : `/${repoName}/`,
 });
